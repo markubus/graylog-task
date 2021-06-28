cc7852610aa9686cee4bd2346b8e5242dc9a31d4
Markus <markubus@gmail.com>
2021-06-28 08:47:27 -0400

    Adjusted spacing

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 # Define the VPC and subnets for use with ECS
@@ -25,8 +25,8 @@ resource "aws_ecs_cluster" "grafana" {
 
 # Defining the task
 resource "aws_ecs_task_definition" "deploy_task" {
-  family                   = "grafana-task"
-  container_definitions    = jsonencode([
+  family = "grafana-task"
+  container_definitions = jsonencode([
     {
       "name": "grafana-task",
       "image": "grafana/grafana",
@@ -42,15 +42,15 @@ resource "aws_ecs_task_definition" "deploy_task" {
     }
   ])
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc"
-  memory                   = 512
-  cpu                      = 256
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  network_mode = "awsvpc"
+  memory = 512
+  cpu = 256
+  execution_role_arn = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
 
 # Define IAM role and policy
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
@@ -59,20 +59,20 @@ data "aws_iam_policy_document" "assume_role_policy" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type        = "Service"
+      type = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  role = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # Defining the load balancer
 resource "aws_lb" "application_lb" {
-  name               = "grafana-task"
+  name = "grafana-task"
   lb_type = "application"
   subnets = [ # Referencing the default subnets
     "${aws_default_subnet.default_subnet_a.id}",
@@ -85,56 +85,56 @@ resource "aws_lb" "application_lb" {
 # Creating a security group for the load balancer
 resource "aws_security_group" "lb_security_group" {
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_lb_target_group" "target_group" {
-  name        = "target-group"
-  port        = 80
-  protocol    = "HTTP"
+  name = "target-group"
+  port = 80
+  protocol = "HTTP"
   target_type = "ip"
-  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+  vpc_id = "${aws_default_vpc.default_vpc.id}"
 }
 
 resource "aws_lb_listener" "listener" {
   lb_arn = "${aws_lb.application_lb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
+  port = "80"
+  protocol = "HTTP"
   default_action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = "${aws_lb_target_group.target_group.arn}"
   }
 }
 
 # Defining the service
 resource "aws_ecs_service" "grafana_service" {
-  name            = "grafana-service"
-  cluster         = "${aws_ecs_cluster.grafana.id}"
+  name = "grafana-service"
+  cluster = "${aws_ecs_cluster.grafana.id}"
   task_definition = "${aws_ecs_task_definition.deploy_task.arn}"
-  launch_type     = "FARGATE"
-  desired_count   = 3
+  launch_type = "FARGATE"
+  desired_count = 3
 
   lb {
     target_group_arn = "${aws_lb_target_group.target_group.arn}"
-    container_name   = "${aws_ecs_task_definition.deploy_task.family}"
-    container_port   = 3000
+    container_name = "${aws_ecs_task_definition.deploy_task.family}"
+    container_port = 3000
   }
 
   network_configuration {
-    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    subnets = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
     assign_public_ip = true
-    security_groups  = ["${aws_security_group.service_security_group.id}"]
+    security_groups = ["${aws_security_group.service_security_group.id}"]
   }
 }
 
@@ -142,15 +142,15 @@ resource "aws_ecs_service" "grafana_service" {
 resource "aws_security_group" "service_security_group" {
   ingress {
     from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    to_port = 0
+    protocol = "-1"
     security_groups = ["${aws_security_group.lb_security_group.id}"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
